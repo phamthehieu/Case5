3d5e6fcfca44624cd5afd66d9aa642b56304f9c5
Đamthanhtung <tungdtha140465@fpt.edu.vn>
2023-02-28 14:16:18 +0700

    tung push

--- src/router/router.ts
+++ src/router/router.ts
@@ -7,7 +7,7 @@ import {postRouter} from "./postRouter";
 import {friendsRouter} from "./friendsRouter";
 export const router = Router()
 router.use('/auth', authRouter)
-router.use(auth)
+// router.use(auth)
 router.use('/users', userRouter)
 router.use('/comment', commentRouter)
 router.use('/posts', postRouter)
